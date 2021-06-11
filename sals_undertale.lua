b5250734597e8d97cfc6292e4674a85bf88bbf80
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-06-10 21:36:14 -0600

    Update sals_undertale.lua

--- sals_undertale.lua
+++ sals_undertale.lua
@@ -9,6 +9,18 @@ Content.AddCharacterDef
         title = "Karmic Executioner",
 
         bio = "If you see Sals Undertale and she looks at your general direction, it is already too late. Just restart the run.",
+
+        fight_data =
+        {
+            MAX_HEALTH = 65,
+            MAX_MORALE = MAX_MORALE_LOOKUP.HIGH,
+
+            behaviour = {
+                OnActivate = function( self, fighter )
+                    self.fighter.stat_bounds[ COMBAT_STAT.HEALTH ].min = 1 -- cannot be killed
+                end,
+            },
+        },
     })
 )
 Content.GetCharacterDef("SALS_UNDERTALE"):InheritBaseDef()
